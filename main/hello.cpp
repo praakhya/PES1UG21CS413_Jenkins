0e19df9e21e4867c2c3eaaafccf27cc8a85a9290
praakhya <67508200+praakhya@users.noreply.github.com>
2024-03-04 18:10:51 +0530

    Jenkin_main files added

---
+++ main/hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I, PES1UG21CS413 Praakhya Avasthi, have successfully built and run\n";
+  return 0;
+}
